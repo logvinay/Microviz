4fc8f20bcfd7eb14744a71e393f44ffb310d8a43
Vinay Kumar <vinayyadav0530@gmail.com>
2021-12-03 01:47:11 +0530

    Added swagger support in production

--- Microviz/Program.cs
+++ Microviz/Program.cs
@@ -12,11 +12,11 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 
 {
-    if (app.Environment.IsDevelopment())
-    {
+    //if (app.Environment.IsDevelopment())
+    //{
         app.UseSwagger();
         app.UseSwaggerUI();
-    }
+    //}
     app.UseHttpsRedirection();
     app.UseRouting();
     app.UseAuthorization();
